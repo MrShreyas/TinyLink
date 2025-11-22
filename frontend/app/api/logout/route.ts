5f5aa66f818f7cedf566d803f77a97980bff41fd
Shreyas <katalkarshreyas@gmail.com>
2025-11-22 23:46:30 +0530

    added login error

--- frontend/app/api/logout/route.ts
+++ frontend/app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 
 
-export default async function POST(req: Request) {
+export async function POST(req: Request) {
     const response = NextResponse.json({ message: 'Login successful' });
     response.cookies.set('auth_token', '', { httpOnly: true, secure: true, maxAge: 0 });
 
